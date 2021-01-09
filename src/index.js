ab2fab2504681728a20473dccebd2c59c84c50a4
Unknown <Unknown>
2021-01-08 19:11:12 -0600

    redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-import { Provider } from 'react-redux'
-import { combineReducers, createStore } from 'redux'
-
-import counterReducer from './store/reducers/counter'
-import resultReducer from './store/reducers/result'
-
-const rootReducer = combineReducers({
-    ctr: counterReducer,
-    res: resultReducer
-})
-
-const store = createStore(rootReducer)
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
