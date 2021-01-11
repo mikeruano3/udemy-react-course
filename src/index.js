2ef5c07792d5d625d6b58835f3c42689a36f93b4
Unknown <Unknown>
2021-01-11 10:35:57 -0600

    Almost complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import AuthContextProvider from './context/auth-context'
+import ProductsProvider from './context/products-context'
 
 ReactDOM.render(
-    <AuthContextProvider>
-        <App />
-    </AuthContextProvider>, 
-document.getElementById('root'));
+  <ProductsProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductsProvider>,
+  document.getElementById('root')
+);
