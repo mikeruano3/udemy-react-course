5d1e415fc68f38e6ec24687efce2664a2dd08942
Unknown <Unknown>
2021-01-17 12:02:07 -0600

    Burger #2

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Modal.css'
-import Aux from '../../../hoc/Aux_'
+import Aux from '../../../hoc/Aux_/Aux_'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-     <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextStage) {
+        return nextProps.show !== this.props.show
+    }
+
+    componentDidUpdate() {
+        console.log('[Modal Will Update]')
+    }
+
+    render() {
+        return <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
         <div 
             className={classes.Modal}
             style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
+                transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: this.props.show ? '1' : '0'
             }}>
-            {props.children}
+            {this.props.children}
         </div>
     </Aux>
-)
+    }
+}
 
-export default modal
+export default Modal
