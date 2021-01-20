1839440e8124b6518338310b75eca1d97679bfec
Unknown <Unknown>
2021-01-20 09:24:37 -0600

    Assignment 3 done

--- src/App.js
+++ src/App.js
@@ -1,46 +1,52 @@
 import React, { Component } from 'react';
-import './App.css';
-import ValidationComponent from './ValidationComponent'
-import CharComponent from './CharComponent'
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from 'react-router-dom';
+
+import Courses from './containers/Courses/Courses';
+//import Course from './containers/Course/Course';
+import Users from './containers/Users/Users';
 
 class App extends Component {
-  state = {
-    inputWord: ''
-  }
-
-  setWord = (event) => {
-    this.setState({inputWord: event.target.value})
-  }
-
-  handleClickOnParagraph = (index) =>{
-    //const newString = this.state.inputWord.split('').filter( (value, idx) => idx !== index )
-    const st = this.state.inputWord.split('')
-    st.splice(index, 1)
-    this.setState({ inputWord: st.join('')})
-  }
-
   render() {
     return (
-      <div className="App">
-        <input type="text" onChange={this.setWord} value={this.state.inputWord}/>
-        <ValidationComponent size={this.state.inputWord.length} />
-        {
-          this.state.inputWord.split('').map((value, index) =>{
-            return <CharComponent paragraph={value} key={index} onClick={()=>this.handleClickOnParagraph(index)} />
-          })
-        }
-        <ol>
-          {/*
-            <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
-          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
-          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
-          */}
-        </ol>
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
-      </div>
+      <Router>
+        <div className="App">
+
+          <ol style={{ textAlign: 'left' }}>
+            <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
+            <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
+            <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
+            <li>Pass the course ID to the "Course" page and output it there</li>
+            <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
+            <li>Load the "Course" component as a nested component of "Courses"</li>
+            <li>Add a 404 error page and render it for any unknown routes</li>
+            <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
+          </ol>
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+                <li>
+                  <Link to="/courses">Courses</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          
+          
+
+          <Switch>
+            <Route path="/users" component={Users} />
+            {//<Route path="/courses/:id/:title" component={Course} />
+            }
+            <Route path="/courses" component={Courses} />
+            <Redirect from="/all-courses" to="/courses" />
+            <Redirect from="/" exact to="/courses" />
+            <Route render={() => <h1>Not found</h1>} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
