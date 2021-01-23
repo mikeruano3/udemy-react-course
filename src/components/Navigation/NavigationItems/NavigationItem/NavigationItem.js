f989eca9e306ebc2c19f87d7c8c0f10321d4be6f
Unknown <Unknown>
2021-01-22 23:19:42 -0600

    Done

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
 import classes from './NavigationItem.css'
+import { NavLink } from 'react-router-dom'
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <a href={props.link} 
-            className={props.active ? classes.active : null}>
+        {/*className={props.active ? classes.active : null*/}
+        <NavLink 
+            to={props.link}
+            exact
+            activeClassName={classes.active}>
             {props.children}
-        </a>
+        </NavLink>
     </li>
 )
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,7 @@ const INGREDIENT_PRICES = {
 class BurguerBuilder extends Component{
     state = {
         ingredients: null,
-        totalPrice: 4,
+        totalPrice: 0,
         purchasable: false,
         purchasing: false,
         loading: false,
@@ -85,28 +85,16 @@ class BurguerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Miguel Ruano',
-                address: {
-                    street: 'TestStreet 1',
-                    zipCode: '41351',
-                    country: 'Guatemala'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false})
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false})
-            })
+       const queryParams = []
+       for (let i in this.state.ingredients){
+           queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+       }
+       queryParams.push('price=' + this.state.totalPrice)
+       const queryString = queryParams.join('&')
+       this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        })
     }
 
     render () {
