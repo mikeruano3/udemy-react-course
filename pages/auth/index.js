3a4589f70dbd19b3196a913d0e9e78361b86f221
Unknown <Unknown>
2021-01-29 22:10:52 -0600

    next finished

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 import User from '../../components/User'
 
-const authIndexPage = () => (
+const AuthIndexPage = (props) => (
     <div>
-        <h1>The Auth Index Page</h1>
+        <h1>The Auth Index Page from {props.appName}</h1>
         <User name="Max" age={28} />
     </div>
 )
 
-export default authIndexPage
+AuthIndexPage.getInitialProps = async (context) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: 'Super App Auth' })
+        }, 2000)
+    })
+    return await promise
+}
+
+export default AuthIndexPage
