43bcbea28b301e087193149afcd920cd3fca4273
Unknown <Unknown>
2021-02-04 16:48:12 -0600

    Custom hooks done

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -4,6 +4,7 @@ import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList'
 import ErrorModal from '../UI/ErrorModal'
 import Search from './Search';
+import useHttp from '../../hooks/http'
 
 const ingredientReducer = (currentIngredients, action) => {
   switch (action.type) {
@@ -18,44 +19,12 @@ const ingredientReducer = (currentIngredients, action) => {
   }
 }
 
-const httpReducer = (curHttpState, action) => {
-  switch (action.type) {
-    case 'SEND':
-      return { loading: true, error: null }
-    case 'RESPONSE':
-      return { ...curHttpState, loading: false }
-    case 'ERROR':
-      return { loading: false, error: action.errorMessage }
-    case 'CLEAR':
-      return { ...curHttpState, error: null }
-    default:
-      throw new Error('Should not be reached')
-  }
-}
-
 const Ingredients = () => {
-  const [userIngredients, dispatchAction] = useReducer(ingredientReducer, [])
-  const [ httpState, dispatchHttp ] = useReducer(httpReducer, {laoding: false, error: null})
-  //const [ userIngredients, setUserIngredients ] = useState([])
-  //const [ isLoading, setIsLoading ] = useState(false)
-  //const [ error, setError ] = useState(false)
+  const [userIngredients, dispatchAction] = useReducer(ingredientReducer, [])  
+  const { isLoading, error, data, sendRequest, reqExtra, reqIdentifier, clear } = useHttp()
+
 
   useEffect(() => {
-    /*
-    fetch('https://hooks-test-f2780-default-rtdb.firebaseio.com/ingredients.json')
-    .then(response => response.json())
-    .then(responseData => {
-      const loadedIngredients = []
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount
-        })
-      }
-      setUserIngredients(loadedIngredients)
-    })*/
-    
   }, []);
 
   useEffect(() => {
@@ -66,61 +35,42 @@ const Ingredients = () => {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
 
+  useEffect(() => {
+    if (!isLoading && !error && reqIdentifier === 'REMOVE_INGREDIENT') {
+      dispatchAction({ type: 'DELETE', id: reqExtra })
+    } else if (!isLoading && !error && reqIdentifier === 'ADD_INGREDIENT'){
+      dispatchAction({ type: 'ADD', 
+        ingredient: { id: data.name, ...reqExtra}})
+    }
+  }, [data, reqExtra, reqIdentifier, isLoading, error]);
+
   const filteredIngredientsHandler = useCallback(filteredIngredients => (
-    //setUserIngredients(filteredIngredients)
     dispatchAction({ type: 'SET', ingredients: filteredIngredients})
   ), [])
 
   const addIngredientHandler = useCallback(ingredient => {
-    //setIsLoading(true)
-    dispatchHttp({ type: 'SEND' })
-    fetch('https://hooks-test-f2780-default-rtdb.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: { 'Content-Type': 'application/json' }
-    })
-    .then(response => {
-      //setIsLoading(false)
-      dispatchHttp({ type: 'RESPONSE' })
-      return response.json()
-    })
-    .then(response => {
-      /*setUserIngredients(prevIngredients => 
-        [...prevIngredients, 
-          { id: response.name, ...ingredient}]
-      )*/
-      dispatchAction({ type: 'ADD', ingredient: { id: response.name, ...ingredient}})
-    }).catch(error => {
-      //setIsLoading(false)
-      dispatchHttp({ type: 'ERROR', errorMessage: error.message })
-    })
-  }, [])
+    sendRequest('https://hooks-test-f2780-default-rtdb.firebaseio.com/ingredients.json',
+      'POST', 
+      JSON.stringify(ingredient),
+      ingredient,
+      'ADD_INGREDIENT'
+      )
+    }, [sendRequest])
 
   const removeIngredientHandler = useCallback(ingredientId => {
-    //setIsLoading(true)
-    dispatchHttp({ type: 'SEND' })
-    fetch(`https://hooks-test-f2780-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE'
-    })
-    .then(response => {
-      //setIsLoading(false)
-      dispatchHttp({ type: 'RESPONSE' })
-      /*setUserIngredients(prevIngredients => 
-        prevIngredients.filter(ing => ing.id !== ingredientId)  
-      )*/
-      dispatchAction({ type: 'DELETE', id: ingredientId })
-    }).catch(error => {
-      //setError(error.message)
-      //setIsLoading(false)
-      dispatchHttp({ type: 'ERROR', errorMessage: error.message })
-    })
-    
-  }, [])
+    sendRequest(`https://hooks-test-f2780-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+    'DELETE',
+    null,
+    ingredientId,
+    'REMOVE_INGREDIENT')
+  }, [sendRequest])
 
+  /*
   const clearError = useCallback(() => {
     //setError(null)
-    dispatchHttp({ type: 'CLEAR' })
+    //dispatchHttp({ type: 'CLEAR' })
   }, [])
+  */
 
   const ingredientList = useMemo(() => {
     return <IngredientList ingredients={userIngredients} 
@@ -130,9 +80,9 @@ const Ingredients = () => {
 
   return (
     <div className="App">
-      {httpState.error && <ErrorModal onClose={clearError}>
-        {httpState.error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading}/>
+      {error && <ErrorModal onClose={clear}>
+        {error}</ErrorModal>}
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal'
 import './Search.css';
+import useHttp from '../../hooks/http'
 
 const Search = React.memo(props => {
   const { onLoadIngredients } = props
   const [enteredFilter, setEnteredFilter] = useState('');
   const inputRef = useRef()
+  const { isLoading, error, data, sendRequest, clear } = useHttp()
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -16,32 +19,37 @@ const Search = React.memo(props => {
           ? ''
           : `?orderBy="title"&equalTo="${enteredFilter}"`
 
-        fetch('https://hooks-test-f2780-default-rtdb.firebaseio.com/ingredients.json' + query)
-          .then(response => response.json())
-          .then(responseData => {
-            const loadedIngredients = []
-            for (const key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount
-              })
-            }
-            onLoadIngredients(loadedIngredients)
-          })
+        sendRequest('https://hooks-test-f2780-default-rtdb.firebaseio.com/ingredients.json' + query, 
+          'GET')
       }
     }, 700)
-
     return () => {
       clearTimeout(timer)
     }
-  }, [enteredFilter, onLoadIngredients, inputRef]);
+  }, [enteredFilter, inputRef, sendRequest]);
+
+  useEffect(() => {
+    if(!isLoading && !error && data){
+      const loadedIngredients = []
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        })
+      }
+      onLoadIngredients(loadedIngredients)
+    }
+    
+  }, [data, isLoading, error, onLoadIngredients]);
 
   return (
     <section className="search">
+      { error && <ErrorModal onClose={clear}>{error}</ErrorModal> }
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input type="text"
             ref={inputRef}
             value={enteredFilter}
