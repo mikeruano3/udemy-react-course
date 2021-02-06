ce05ca255f2f63246813be0efc8cbfa6151d725e
Miguel Ruano <miguelruano35@gmail.com>
2021-02-05 21:08:42 -0600

    redux hooks

--- src/index.js
+++ src/index.js
@@ -1,41 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
-import createSagaMiddleware from 'redux-saga'
+import { Provider } from 'react-redux';
+import { combineReducers, createStore } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import burgerBuilderReducer from './store/reducers/burgerBuilder'
-import orderReducer from './store/reducers/order'
-import authReducer from './store/reducers/auth'
-import { watchAuth, watchBurgerBuilder, watchOrder } from './store/sagas'
-
-const composeEnhancers = (typeof window !== 'undefined' && process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null ) || compose;
+import productReducer from './store/reducers/products';
 
 const rootReducer = combineReducers({
-    burgerBuilder: burgerBuilderReducer,
-    order: orderReducer,
-    auth: authReducer
-})
+  shop: productReducer
+});
 
-const sagaMiddleware = createSagaMiddleware()
-
-const store = createStore(rootReducer,
-    composeEnhancers(applyMiddleware(thunk, sagaMiddleware)))
-
-sagaMiddleware.run(watchAuth)
-sagaMiddleware.run(watchBurgerBuilder)
-sagaMiddleware.run(watchOrder)
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
-    , document.getElementById('root'));
-registerServiceWorker();
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
